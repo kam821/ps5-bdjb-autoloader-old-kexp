241fc063f24bde679abced12755934d18f68af0d
najdek <4890607+najdek@users.noreply.github.com>
2026-05-23 14:39:42 +0200

    use custom elfldr.elf source: https://github.com/itsPLK/elfldr

--- payloads/poops/src/org/bdj/external/AioShellcode.java
+++ payloads/poops/src/org/bdj/external/AioShellcode.java
@@ -22,7 +22,7 @@ public class AioShellcode {
     private static final int MAP_SHARED = 0x0001;
 
     private static final String BIN_PATH = "/org/bdj/external/kexp_v6.bin";
-    private static final String ELFLDR_PATH = "/org/bdj/external/elfldr_1320_v5.elf";
+    private static final String ELFLDR_PATH = "/org/bdj/external/elfldr.elf";
 
     private static final API       api;
     private static final KernelAPI kapi;
